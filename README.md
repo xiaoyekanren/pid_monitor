109d6126529b418038cd0b886fa051268ef64e9a
xiaoyekanren <876670773@qq.com>
2020-05-19 09:31:28 +0800

    update monitor_to_csv.py

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
 "# pid_monitor" 
+# python version
 python3
+# 依赖
+```python
+import time
+import csv
+import psutil
+import argparse
+```
+# 功能
+大概就是根据pid来监控使用内存、CPU，直到该PID消失
+
+画图，考虑了下还是拿tableau画比较直观，就不用matplotlib.pyplot了
--- monitor_to_csv.py
+++ monitor_to_csv.py
@@ -15,22 +15,27 @@ if not interval:
 if not pid:
     print('please input a pid to monitor')
     exit()
-
 proc = psutil.Process(int(pid))
-f = open('pid.csv', 'w')
-f.truncate()
-f_csv = csv.writer(f)
 
+# # 与下面貌似一样，这个不需要关闭，下面的还得手动close
+# # 这个只是不需要close了
+# with open('pid.csv', 'w', newline='') as f:
+#     f_csv = csv.writer(f)
+#     while True:
+#         if not psutil.pid_exists(pid):
+#             f.close()
+#             exit(0)
+#         row = [pid, time.strftime("%Y-%m-%d %H:%M:%S"), proc.memory_percent(), proc.cpu_percent(), psutil.net_io_counters().bytes_recv, psutil.net_io_counters().bytes_sent, psutil.net_io_counters().packets_recv, psutil.net_io_counters().packets_sent]
+#         f_csv.writerow(row)
+#         time.sleep(interval)
+
+# 同上...
+f = open('pid.csv', 'w', newline='')
+f_csv = csv.writer(f)
 while True:
-    row = []
-    row = [pid, time.strftime("%Y-%m-%d %H:%M:%S"), proc.memory_percent(), proc.cpu_percent(), psutil.net_io_counters().bytes_recv, psutil.net_io_counters().bytes_sent, psutil.net_io_counters().packets_recv, psutil.net_io_counters().packets_sent]
-    f_csv.writerow(row)
     if not psutil.pid_exists(pid):
         f.close()
         exit(0)
-    time.sleep(0.01)
-f.close()
-
-
-
-
+    row = [pid, time.strftime("%Y-%m-%d %H:%M:%S"), proc.memory_percent(), proc.cpu_percent(), psutil.net_io_counters().bytes_recv, psutil.net_io_counters().bytes_sent, psutil.net_io_counters().packets_recv, psutil.net_io_counters().packets_sent]
+    f_csv.writerow(row)
+    time.sleep(interval)
